c4de624d541edac1768b13dbe7a452df59a0c0c5
KirillAtrakhimovich <kirill.atrakhimovich@gmail.com>
2026-02-03 22:12:22 +0300

    Initial Commit

---
+++ HabitFlow/HabitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlowApp.swift
+//  HabitFlow
+//
+//  Created by Kirill Atrakhimovich on 3.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
